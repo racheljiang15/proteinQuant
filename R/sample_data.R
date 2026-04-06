c01dc3f5e0a4bf2f5dc6498ff6b3392845c2111c
ay-cheng <aycheng@smith.edu>
2026-04-06 11:47:36 -0400

    updates

---
+++ R/sample_data.R
@@ -0,0 +1,33 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+sample_data <- function (x){
+  data  <- usethis::use_data(x)
+  # data processing
+}
+
+# sample_plate_reader <- usethis::use_data(x)
+
+
+
+
+
+
+
+
+
+
+
