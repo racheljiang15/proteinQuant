2204571d021808a1af774159a0dc2fd193904093
racheljiang15 <rjiang@smith.edu>
2026-04-06 12:07:14 -0400

    test

---
+++ R/sample_data.R
@@ -0,0 +1,38 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+<<<<<<< HEAD:R/sample_plate_reader.R
+sample_plate_reader <- function (x, absorbance, concentration){
+  plate_data  <- usethis::use_data(x)
+=======
+sample_data <- function (x){
+  data  <- usethis::use_data(x)
+>>>>>>> c01dc3f5e0a4bf2f5dc6498ff6b3392845c2111c:R/sample_data.R
+  # data processing
+}
+
+# sample_plate_reader <- usethis::use_data(x)
+
+
+
+
+
+
+
+
+
+
+
