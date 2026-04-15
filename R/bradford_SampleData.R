cac042ee3739e6589f1a12b7fdc8647148401bfa
ay-cheng <aycheng@smith.edu>
2026-04-15 16:44:33 -0400

    splitting data into separate R files with new documentation- AYC

---
+++ R/bradford_SampleData.R
@@ -0,0 +1,14 @@
+#' Sample Bradford Absorbances
+#'
+#' @description The data was collected in an introductory Biochemistry lab course at Smith College in the Spring 2026 semester. It includes absorbance data taken from performing a Bradford assay on 7 test protein solutions of unknown concentration.
+#' @format Data frame with absorbance values.
+#' @export 
+#' @source R. Jiang and A.Y. Cheng (2026), BCH 253-01 Smith College
+#' @examples
+#' bradford_sample_data
+#'
+#' @name bradford_SampleData
+
+bradford_sample_data
+#bradford_sample_data <- read.csv("bradford_sample_data.csv")
+#usethis::use_data(bradford_sample_data, overwrite = TRUE)
