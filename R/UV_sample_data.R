df0c39f7b2dea27f22ba3a07d1d0b77cb64d2465
racheljiang15 <rjiang@smith.edu>
2026-04-22 11:18:19 -0400

    added new data

---
+++ R/UV_sample_data.R
@@ -0,0 +1,13 @@
+#' Protein Quantification Test UV Sample
+#'
+#' @description The data was collected in an introductory Biochemistry lab course at Smith College in the Spring 2026 semester. It includes absorbance data used in the creation of a BSA standard curve for the UV280 protein quantification method.
+#' @format Data frame with concentration and absorbance values.
+#' @source R. Jiang and A.Y. Cheng (2026), BCH 253-01 Smith College
+#' @examples
+#' UV_sample_data
+#'
+#' @name UV_sample_data
+
+"UV_sample_data"
+#UV_sample_data <- read.csv("UV_sample_data.csv")
+#usethis::use_data(UV_sample_data, overwrite = TRUE)
