c646560d98db428753f128fecbddb12ef34f4f46
ay-cheng <aycheng@smith.edu>
2026-04-30 19:27:38 -0400

    adding more tests- AYC

--- tests/testthat/test-UV_graphing_tool.R
+++ tests/testthat/test-UV_graphing_tool.R
@@ -1,4 +1,4 @@
-test_that("assay_graphing_tool returns a ggplot object", {
+test_that("UV_graphing_tool returns a ggplot object", {
   test_data <- data.frame(
     conc = c(1, 2, 3, 4),
     abs = c(0.1, 0.2, 0.3, 0.4)
